04886ea8b8a93a366c52819f0e720337d2dcf5a3
Bianca Nobrega <bmoura@live.com>
2018-03-20 15:42:25 +0000

    Array de usuários que será utilizado na primeira aula.

---
+++ aula1/usuarios.js
@@ -0,0 +1,19 @@
+const usuarios = [
+  {
+    id: 1,
+    nome: 'Maria',
+    sexo: 'Feminino'
+  },
+  {
+    id: 2,
+    nome: 'Joaquim',
+    sexo: 'Masculino'
+  },
+  {
+    id: 3,
+    nome: 'Pedro',
+    sexo: 'Masculino'
+  }
+];
+
+module.exports = usuarios;
